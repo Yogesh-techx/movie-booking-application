e11fe7fbea8418d6264a65eb3d34a2869425cdfd
Yogesh Joshi <yogeshjoshi0325@gmail.com>
2023-11-17 09:00:05 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # movie-booking-application
+# movie-booking-application
--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,44 @@
-import express from "express";
-import userRouter from "./routes/user-routes";
-// import mongoose from "mongoose";
-// const app = express();
-// import dotenv from 'dotenv'
-// const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const userRouter = require('./routes/user-routes');
+const adminRouter = require('./routes/admin-routes');
+const movieRouter = require('./routes/movie-routes');
+const bookingRouter = require('./routes/booking-routes');
+const fs = require('fs');
+dotenv.config();
 
-//middlewares
-app.use("/user", UserRouter)
+const PORT = process.env.PORT || 4500;
+
+const cors = require('cors');
+app.use(cors());
+
+// Set up CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// Middleware section
+app.use(express.json());
+app.use("/users", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movies", movieRouter);
+app.use("/booking", bookingRouter);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
 
 
-mongoose.connect(`mongodb+srv://myselfyogesh25:${process.env.MONGODB_PASSWORD}@cluster0.jwiwd8d.mongodb.net/
-`)
-    .then(() => app.listen(4000, () => console.log("Connected Successfully")
-    )
-    ).catch(e => console.log(e));
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB.');
+});
 
-
+app.listen(PORT, () => {
+    console.log(`Database Connected. Server is running on port ${PORT}`);
+});
